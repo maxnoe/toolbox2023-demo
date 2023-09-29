fac7cf3f774300ca5eafb908d98c7fbad71e759a
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-09-29 15:51:53 +0200

    Use LaTeX in axis labels

--- plot_sine.py
+++ plot_sine.py
@@ -1,15 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 1, 1000)
+x = np.linspace(0, 1, 100)
 
 fig, ax = plt.subplots(layout="constrained")
 
-ax.plot(x, np.sin(4 * np.pi * x), color='red', marker='.')
+ax.plot(
+    x,
+    np.sin(4 * np.pi * x),
+    color='red',
+    marker='.',
+    linestyle='none',
+)
 
 ax.set(
-    xlabel="t / s",
-    ylabel="U / V",
+    xlabel=r"$t / \mathrm{s}$",
+    ylabel=r"$U / \mathrm{V}$",
     title="Sine",
 )
 
