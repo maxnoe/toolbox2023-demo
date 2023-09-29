29f806263a90147b83e49da203cb5a4ab70abaea
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-09-29 14:42:02 +0200

    Make it run fast

--- fib.py
+++ fib.py
@@ -1,10 +1,19 @@
-def fibonacci(n):
-    if n < 2:
+def fibonacci(i):
+    n = 0
+    if i == 0:
         return n
+
+    previous = 0
+    n = 1
+    if i == 1:
+        return n
+
+    for _ in range(2, i):
+        n, previous = n + previous, n
     
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return n
 
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(35):
         print(fibonacci(i))
